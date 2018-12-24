226b6966767f7c7c2466c8f18f71b4efc9cdbf36
shannonmoranetz <shannonrmoranetz@gmail.com>
2018-12-24 14:07:53 -0700

    Updates

---
+++ precache-manifest.37af0116e06a0d80ec03d9b20d4e3f61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d75e5a2793756dba8dc",
+    "url": "/ctrl-app/static/css/main.c759d676.chunk.css"
+  },
+  {
+    "revision": "8d75e5a2793756dba8dc",
+    "url": "/ctrl-app/static/js/main.8d75e5a2.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/ctrl-app/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "663f65f64fc2e9250c61",
+    "url": "/ctrl-app/static/js/runtime~main.663f65f6.js"
+  },
+  {
+    "revision": "bdc26de213e659e3d3bac796081bcc02",
+    "url": "/ctrl-app/static/media/bubble.bdc26de2.png"
+  },
+  {
+    "revision": "bd05c8c76704a1673b335fdd4a5d58af",
+    "url": "/ctrl-app/static/media/app-bg.bd05c8c7.jpg"
+  },
+  {
+    "revision": "0f3bf441c7205e5e9ca0f785e5de46a7",
+    "url": "/ctrl-app/static/media/splash-bg.0f3bf441.jpg"
+  },
+  {
+    "revision": "2aea5154d82cc52dd7772b9d72421341",
+    "url": "/ctrl-app/index.html"
+  }
+];
