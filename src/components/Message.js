09bef3ba56e03494dcc07a9e427e4864f0987000
shannonmoranetz <shannonrmoranetz@gmail.com>
2018-12-24 03:18:18 -0700

    Add initial message logic

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
+import messageData from '../../src/messageData.js';
 import '../../src/styles/main.scss';
 
-export default function Message() {
+export default class Message extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      currentMessage: '',
+      allMessages: []
+    }
+  }; 
 
-  return(
-    <div className="message-container">
-      <h2 className="message-text">message</h2>
-    </div>
-  )
+  componentWillMount() {
+    this.startTimer()
+    this.createMsgArray()
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  createMsgArray() {
+    let messageArray = Object.keys(messageData).map((msg) => {
+      return msg.message;
+    })
+    this.setState({
+      allMessages: messageArray
+    });
+  }
+
+
+
+
+
+
+
+
+
+  tick() {
+    this.setState({
+      count: (this.state.count + 1),
+    })
+  }
+
+  startTimer() {
+    clearInterval(this.timer)
+    this.timer = setInterval(this.tick.bind(this), 1000)
+  }
+
+  
+
+
+  
+  // generateRandomMessage() {
+  //   const newMessage = this.state.messageData[Math.floor(Math.random() * this.state.messageData.length)];
+  //   this.updateMessage(newMessage)
+  // }
+
+  // updateMessage(message) {
+  //   this.setState({
+  //     currentMessage: message
+  //   });
+  // }
+
+  render() {
+    return(
+      <div className="message-container">
+        <h2 className="message-text">joajwoopawje</h2>
+      </div>
+    )
+  };
 };
 
